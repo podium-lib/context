aa9199aae33e7c48b12b75ab47bbb985137b32a6
Simen Bekkhus <simen.bekkhus@finn.no>
2017-09-12 15:20:03 +0200

    Explicitly pass through config (#135)
    * Export config from data-services
    
    * Fix typ0 in podium-resource url
    
    * Add brakes logging
    
    * Do not re-use middleware
    
    * Do not throw if validate-token fails
    
    * Define resourceMountPath
    
    * Pass config obj around instead of destructure
    
    * Retry entrypoint upload and reject when fallback from podlet
    
    * Add config exports
    
    * Add delay before retry
    
    * Use config object in resource proxy
    
    * Do not send the reason/error obj
    
    * Fix tests for pipe service
    
    * use t.throws over try-catch in pipe-service tests
    
    * Cleanup horrible
    
    * Fix podium client config usage
    
    * Cleanup config usage in tests
    
    * Fix user-searches podlet
    
    * Pick up context stuff from config
    
    * Fix market-icons podlet
    
    * Fix global-search podlet
    
    * Fix recommendations podlet
    
    * Add assetPipeServerUrl to host-middleware
    
    * Cleanup config usage
    
    * Cleanup config
    
    * 	Try test invalid in package tree
    
    * Dedupe envalid

---
+++ config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const dataServicesConfig = require('@finn-no/data-services/config');
+
+const { str, url } = require('envalid');
+
+module.exports = Object.assign(
+    {},
+    {
+        RESOURCE_MOUNT_PATH: str({
+            default: '/podium-resource',
+        }),
+        BASE_URL: str({
+            desc: 'The base URL used by podlets',
+            default: 'https://www.finn.no',
+            devDefault: '',
+        }),
+        CDN_HOST: url({
+            desc: 'The base URL used by podlets',
+            default: 'https://static.finncdn.no',
+        }),
+    },
+    dataServicesConfig
+);
