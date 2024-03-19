6a0215cce6df9bb87732ae38336206ee9614ab9a
Tor Tryggestad Berre <tor.tryggestad.berre@schibsted.com>
2024-03-19 11:04:24 +0100

    feat: add support for x-podium-base-font-size header

---
+++ tests/get-base-font-size.js
@@ -0,0 +1,38 @@
+import tap from 'tap';
+import { HttpIncoming } from '@podium/utils';
+import BaseFontSizeParser from '../lib/get-base-font-size.js';
+
+tap.test(
+    'PodiumContextBaseFontSizeParser() - returns the expected default value',
+    (t) => {
+        const parser = new BaseFontSizeParser();
+        const incoming = new HttpIncoming({
+            headers: {
+                host: 'localhost:3030',
+            },
+            hostname: 'localhost',
+            url: '/some/path',
+        });
+        t.ok(parser instanceof BaseFontSizeParser);
+        t.equal(parser.parse(incoming), '1rem');
+        t.end();
+    },
+);
+
+tap.test(
+    'PodiumContextBaseFontSizeParser() - sets the expected font size from the incoming request',
+    (t) => {
+        const parser = new BaseFontSizeParser();
+        const incoming = new HttpIncoming({
+            headers: {
+                host: 'localhost:3030',
+                'x-podium-base-font-size': '2rem',
+            },
+            hostname: 'localhost',
+            url: '/some/path',
+        });
+        t.ok(parser instanceof BaseFontSizeParser);
+        t.equal(parser.parse(incoming), '2rem');
+        t.end();
+    },
+);
